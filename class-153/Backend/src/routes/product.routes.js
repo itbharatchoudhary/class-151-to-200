63ffd6ceb8f3ad216055841a4822102068f6c3ec
Bharat Choudhary <bharatchoutharyb@gmail.com>
2026-04-23 10:17:55 +0530

    done

---
+++ class-153/Backend/src/routes/product.routes.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { AuthenticateSeller } from "../middleware/auth.middleware.js";
+import { createProduct, getAllProducts, getSellerProducts, getProductDetails,createProductVariant } from "../controllers/product.controller.js";
+import { validateProductCreation } from "../validator/product.validator.js";
+import multer from "multer";
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file size limit
+});
+
+const router = express.Router();
+
+/**
+ * @route POST /products
+ * @desc Create a new product
+ * @access Private (Seller only)
+ * @body { title, description, priceAmount, priceCurrency, images } 
+ */
+router.post("/", AuthenticateSeller, upload.array("images", 7), validateProductCreation, createProduct);
+
+/**
+ * @route GET /products/seller
+ * @desc Get all products for the authenticated seller
+ * @access Private (Seller only)
+ */
+
+router.get("/seller", AuthenticateSeller, getSellerProducts);
+
+/**
+ * @route GET /products
+ * @desc Get all products
+ * @access Public
+ */
+router.get("/",getAllProducts)
+
+/**
+ * @route GET /products/:id
+ * @desc Get product details by ID
+ * @access Public
+ */
+router.get("/:id", getProductDetails) 
+
+/**
+ * 
+ */
+
+router.post("/:id/variants", AuthenticateSeller,upload.array("images", 7), createProductVariant);
+
+export default router;
